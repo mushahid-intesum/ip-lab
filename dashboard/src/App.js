7f8849848f4b216553ba1fafcbee430b6d8469cd
KaziShadmanSakib <shadmansakib.24csedu.010@gmail.com>
2023-04-06 00:25:52 +0600

    UtiliTool initiated.

---
+++ dashboard/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './App.css'
+
+const App = () => {
+  return (
+    <h1 className="underline text-2xl">Hello World! Let's construct UtiliTool from here.</h1>
+  )
+}
+
+export default App
