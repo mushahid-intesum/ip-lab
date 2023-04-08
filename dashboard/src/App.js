897ecdc38e6008023dfd40b6a051a7ed93b34f6b
KaziShadmanSakib <shadmansakib.24csedu.010@gmail.com>
2023-04-08 23:01:06 +0600

    Sidebar added

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,10 +1,59 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { FiSettings } from 'react-icons/fi';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
+import {Navbar, Footer, Sidebar, ThemeSettings} from './components';
+import {Home, Team, Kanban, Calendar} from './pages';
 import './App.css'
 
 const App = () => {
+  const acitiveMenu = true;
   return (
-    <h1 className="underline text-2xl">Hello World! Let's construct UtiliTool from here.</h1>
+    <div>
+      <BrowserRouter>
+        <div className="flex relative  dark:bg-main-dark-bg">
+          {/* Settings */}
+          <div className="fixed right-4 bottom-4" style={{zIndex: '1000'}}>
+            <TooltipComponent content="Settings" position="Top">
+              <button type="button" className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white" style={{ background: 'blue', borderRadius: '50%'}}>
+                <FiSettings />
+              </button>
+            </TooltipComponent>
+          </div>
+          {/* Sidebar */}
+          {acitiveMenu ? (
+            <div className="w-60 fixed sidebar dark:bg-secondary-dark-bg bg-white" >
+              <Sidebar/>
+            </div>
+          ) : (
+            <div className="w-0 dark:bg-secondary-dark-bg">
+              <Sidebar/>
+            </div>
+          )}
+          {/* Navbar */}
+          <div className={ 'dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? "md:ml-72" : "flex-2"}' }>
+            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
+              <Navbar/>
+            </div>
+          </div>
+          <div>
+            <Routes>
+              {/* Dashboard */}
+              <Route path="/" element={<Home />} />
+              <Route path="/home" element={<Home />} />
+              
+              {/* Pages */}
+              <Route path="/team" element={<Team />} />
+              
+              {/* Apps */}
+              <Route path="/kanban" element={<Kanban />} />
+              <Route path="/calendar" element={<Calendar />} />
+            </Routes>
+          </div>
+        </div>
+      </BrowserRouter>
+    </div>
   )
 }
 
---
+++ dashboard/src/pages/index.jsx
@@ -0,0 +1,4 @@
+export { default as Home } from './Home';
+export { default as Kanban } from './Kanban';
+export { default as Team } from './Team';
+export { default as Calendar } from './Calendar';
